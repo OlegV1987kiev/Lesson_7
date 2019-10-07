b217ecff8d94d97512720d9b99252e9d2d94b5a5
Oleg <oleg1987kiev@gmail.com>
2019-10-07 15:41:43 +0300

    tesy

---
+++ src/main/java/Lesson_7_9.java
@@ -0,0 +1,26 @@
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Random;
+
+public class Lesson_7_9 {
+
+    public static void main(String[] args) {
+        Random rd = new Random(); // creating Random object
+        int[] arr = new int[10];
+        int even = 0;
+        int odd = 0;
+        int i = 0;
+        for (i = 0; i < arr.length; i++) {
+            arr[i] = rd.nextInt(100); // storing random integers in an array
+            System.out.println(arr[i]); // printing each array element
+
+
+
+
+        }
+
+
+
+
+    }
+}
